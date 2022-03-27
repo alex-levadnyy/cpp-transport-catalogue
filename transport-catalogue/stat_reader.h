6a99e4d0f9feece3ce3316ec98ccd1e0190072cf
alex-levadnyy <93546496+alex-levadnyy@users.noreply.github.com>
2022-03-27 10:21:12 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,15 @@
-// место для вашего кода
+#pragma once
+
+#include "transport_catalogue.h"
+
+namespace transport_catalogue::stat_reader {
+    void StatReader(TransportCatalogue t);
+
+    void PrintBus(const BusRoute r);
+
+    void PrintStop(const StopRoutes &s);
+
+    std::ostream& operator<<(std::ostream& out, const BusRoute& route_info);
+
+    std::ostream& operator<<(std::ostream& out, const StopRoutes& route_info);
+}
