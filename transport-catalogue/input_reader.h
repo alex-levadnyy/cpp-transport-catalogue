9ddd7efea39ffee97b36359d68522ef522849860
alex-levadnyy <93546496+alex-levadnyy@users.noreply.github.com>
2022-03-27 10:20:24 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,1 +1,15 @@
-// место для вашего кода
+#pragma once
+#include <string>
+#include <deque>
+
+namespace transport_catalogue::input_reader {
+    std::deque<std::string> ReadRequests();
+}
+
+namespace detail {
+    void RemoveBeginEndSpaces(std::string_view& str);
+
+    std::string_view SeparatorString(std::string_view& str, char separator);
+
+    void RemoveBeginSpace(std::string& data);
+}
