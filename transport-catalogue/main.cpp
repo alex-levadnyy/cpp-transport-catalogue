ae5af4044d7a3a43ff2fcbea47e3c81baaf90e34
alex-levadnyy <93546496+alex-levadnyy@users.noreply.github.com>
2022-03-30 19:55:53 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,11 +1,14 @@
-#include "geo.h"
-#include "input_reader.h"
 #include "transport_catalogue.h"
+#include "input_reader.h"
 #include "stat_reader.h"
+#include <iostream>
 
-int main(){
-    using namespace transport_catalogue;
-    TransportCatalogue t(input_reader::ReadRequests());
-    stat_reader::StatReader(t);
-    return 0;
+
+int main() {
+	transport_catalogue::TransportCatalogue catalogue;
+	
+	transport_catalogue::input_reader::ReadRequests(catalogue, std::cin);
+	transport_catalogue::stat_reader::StatReader(catalogue, std::cin, std::cout);
+	
+	return 0;
 }
