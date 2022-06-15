9149382deb66f91627fa24a1f28fbb43d6967a5b
alex-levadnyy <93546496+alex-levadnyy@users.noreply.github.com>
2022-06-15 20:16:59 +0300

    sprint_14

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,20 +1,45 @@
-#include "json_reader.h"
-#include "map_renderer.h"
-#include "request_handler.h"
-
 #include <cassert>
 #include <fstream>
 #include <iostream>
+#include <string_view>
 
-int main()
-{
+#include "request_handler.h"
+
+using namespace std::literals;
+
+void PrintUsage(std::ostream& stream = std::cerr) {
+    stream << "Usage: transport_catalogue [make_base|process_requests]\n"sv;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        PrintUsage();
+        return 1;
+    }
+
+    const std::string_view mode(argv[1]);
     transport_catalogue::TransportCatalogue catalogue;
-    renderer::MapRenderer renderer;
+    transport_catalogue::TransportCatalogueHandler catalogue_handler(catalogue);
+    if (mode == "make_base"sv) {
 
-    json_reader::JsonLoader json(std::cin);
-    json.LoadData(catalogue);
-    json.LoadRenderSettings(renderer);
-    json.AnswerRequests(catalogue, renderer, std::cout);
+        // make base here
+        json_reader::JsonIO json(std::cin);
 
-    transport_catalogue::RequestHandler catalogue_handler(catalogue, renderer);
+        catalogue_handler.LoadDataFromJson(json);
+        catalogue_handler.SerializeData();
+
+    } else if (mode == "process_requests"sv) {
+
+        // process requests here
+        json_reader::JsonIO json(std::cin);
+
+        catalogue_handler.LoadDataFromJson(json);
+        catalogue_handler.DeserializeData();
+
+        catalogue_handler.LoadRequestsAndAnswer(json, std::cout);
+
+    } else {
+        PrintUsage();
+        return 1;
+    }
 }
